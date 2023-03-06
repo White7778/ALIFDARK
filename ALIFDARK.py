dae6d592648feee83f69fc5c5c1d49c81784ce36
White7778 <112792176+White7778@users.noreply.github.com>
2023-03-06 18:40:31 +0600

    Update ALIFDARK.py
    rm -rf ALIFDARK
    git clone https://github.com/White7778/ALIFDARK.git
    ls
    cd ALIFDARK
    python ALIFDARK.py

--- ALIFDARK.py
+++ ALIFDARK.py
@@ -528,7 +528,6 @@ def m(uid,pwx,tl):
             "pass":ps,
             "login":"Log In"}
             headers = {'authority': 'mbasic.facebook.com',}
-            'method': 'GET',
             'scheme': 'https',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
