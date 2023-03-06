06b937c5caadaf9c5430bbd899bb634bb2827143
White7778 <112792176+White7778@users.noreply.github.com>
2023-03-06 08:00:47 +0600

    Update ALIFDARK.py
    rm -rf ALIFDARK
    git clone https://github.com/White7778/ALIFDARK.git
    ls
    cd ALIFDARK
    python ALIFDARK.py

--- ALIFDARK.py
+++ ALIFDARK.py
@@ -541,7 +541,7 @@ def m(uid,pwx,tl):
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',}
+            'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',
             lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
